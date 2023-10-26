8e9cbd0627ba6e033d1b18b150e86868bd903b9c
Sohanur00 <sk1882753729@gmail.com>
2023-10-27 02:31:58 +0600

    server ok

--- src/componants/Home/Services/ServicesCard.jsx
+++ src/componants/Home/Services/ServicesCard.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
 import { FaArrowRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const ServicesCard = ({service}) => {
 
-const {img , title, price} = service;
+const {_id, img , title, price} = service;
 
     return (
         <div className="card w-96 bg-base-100 shadow-xl ">
@@ -14,7 +15,10 @@ const {img , title, price} = service;
           <h2 className="card-title text-4xl font-bold">{title}</h2>
           <p className="text-[#FF3811] font-bold text-2xl">Price:  $ {price}</p>
           <div className="card-actions">
-           <p className='btn'><FaArrowRight></FaArrowRight></p>
+            <Link to={`/checkout/${_id}`}>
+            <button className='btn'><FaArrowRight></FaArrowRight></button>
+            </Link>
+           
           </div>
         </div>
       </div>
