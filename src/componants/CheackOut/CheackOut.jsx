9ff70557d51b1166aa2406b6bbd5f94514dbbeae
Sohanur00 <sk1882753729@gmail.com>
2023-10-29 05:02:50 +0600

    post server connected problem

--- src/componants/CheackOut/CheackOut.jsx
+++ src/componants/CheackOut/CheackOut.jsx
@@ -1,11 +1,67 @@
 import { useLoaderData } from 'react-router-dom';
 import image from '../../assets/images/checkout/checkout.png'
+import { useContext } from 'react';
+import { Authcontext } from '../Provider/AuthProvider';
+
 
 const CheackOut = () => {
 
     const service = useLoaderData()
-    // eslint-disable-next-line no-unused-vars
-    const { title, _id } = service;
+
+    const { title, _id, price, img } = service;
+
+    const { user } = useContext(Authcontext)
+
+    const handleService = event => {
+        event.preventDefault();
+
+        const from = event.target;
+        const name = from.name.value;
+        const date = from.date.value;
+        const phone = from.phone.value;
+        const email = user?.email;
+        const bio = from.bio.value;
+
+
+        const bookings = {
+
+            customer: name,
+            email,
+            date,
+            service: _id,
+            bio,
+            phone,
+            price,
+            img,
+            title
+
+
+
+        }
+        console.log(bookings)
+        fetch('http://localhost:5000/checkout', {
+
+            method: "POST",
+
+            headers: {
+                "content-type": 'applycation/json'
+
+            },
+            body: JSON.stringify(bookings)
+
+
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
+    }
+
+
+
+
 
     return (
         <div className=''>
@@ -18,49 +74,55 @@ const CheackOut = () => {
 
             <h2>CheackOut  services :{title} </h2>
 
-        
-                        <form className="card-body  bg-[#F3F3F3] ">
 
-                        <div className='grid md:grid-cols-2 grid-cols-1 gap-6'>
-                        <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text"> Name</span>
-                                </label>
-                                <input type="text" placeholder=" Name" name='name' className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Date</span>
-                                </label>
-                                <input type="date" placeholder="receving date" className="input input-bordered" name='date'required />
-                               
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Your Phone</span>
-                                </label>
-                                <input type="text" placeholder="Your Phone" className="input input-bordered" name='phone' required />
-                               
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Your Email</span>
-                                </label>
-                                <input type="text" placeholder="Your Email" className="input input-bordered"name='email' required />
-                               
-                            </div>
-                            <div className="form-control col-span-2">
-                            <textarea placeholder="Bio" className="textarea textarea-bordered textarea-lg " ></textarea>
-                            </div>
-                        </div>
+            <form onSubmit={handleService} className="card-body  bg-[#F3F3F3] ">
 
-
-                            <div className="form-control mt-6">
-                                <button className="btn btn-block bg-[#FF3811] text-white">Order Confirm</button>
-                            </div>
-                        </form>
+                <div className='grid md:grid-cols-2 grid-cols-1 gap-6'>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text"> Name</span>
+                        </label>
+                        <input type="text"
+                            defaultValue={user?.displayname}
+                            placeholder=" Name" name='name' className="input input-bordered" required />
                     </div>
-               
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Date</span>
+                        </label>
+                        <input type="date"
+                            defaultValue={user?.date}
+                            placeholder="receving date" className="input input-bordered" name='date' required />
+
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Your Phone</span>
+                        </label>
+                        <input type="text"
+                            defaultValue={user?.phone}
+                            placeholder="Your Phone" className="input input-bordered" name='phone' required />
+
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Your Email</span>
+                        </label>
+                        <input type="text" defaultValue={user?.email} placeholder="Your Email" className="input input-bordered" name='email' required />
+
+                    </div>
+                    <div className="form-control col-span-2">
+                        <textarea placeholder="Bio" name='bio' className="textarea textarea-bordered textarea-lg " ></textarea>
+                    </div>
+                </div>
+
+
+                <div className="form-control mt-6">
+                    <button className="btn btn-block bg-[#FF3811] text-white">Order Confirm</button>
+                </div>
+            </form>
+        </div>
+
     );
 };
 
